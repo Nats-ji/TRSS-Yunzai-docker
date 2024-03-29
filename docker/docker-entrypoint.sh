4b4dd2890f1f5b6683ca93bf6fb1e76f702544b8
Mingming Cui <nats_ji@hotmail.com>
2024-03-30 07:39:19 +0800

    try

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -328,7 +328,7 @@ fi
 echo -e "\n ================ \n ${Info} ${GreenBG} 启动 TRSS-Yunzai ${Font} \n ================ \n"
 
 set +e
-screen -dmS yunzai node .
+node .
 tail -f /dev/null
 EXIT_CODE=$?
 
