276b8aa86cc0796c6c9d52f83b581a87dc7aac19
Mingming Cui <nats_ji@hotmail.com>
2024-03-31 14:21:18 +0800

    add progress to backup

--- backup.sh
+++ backup.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-tar -zcvf "yunzai-backup-$(date '+%Y-%m-%d').tar.gz" ./lagrange ./redis ./yunzai
+if ! [ $(id -u) = 0 ]; then
+   echo "The script needs to be run with sudo." >&2
+   exit 1
+fi
+
+mkdir -p backups
+OUTPUT_NAME="./backups/yunzai-backup-$(date '+%Y-%m-%d').tar.gz"
+TOTAL_SIZE=`du -sk --apparent-size --total lagrange/ redis/ yunzai/ | cut -f 1 | tail -n 1`
+CHECKPOINT=`echo ${TOTAL_SIZE}/50 | bc`
+echo "Creating backup file. Total size: $(numfmt --to=iec $(echo ${TOTAL_SIZE}*1000 | bc))."
+echo "Estimated: [==================================================]"
+echo -n "Progess:   ["
+tar -c --record-size=1K --checkpoint="${CHECKPOINT}" --checkpoint-action="ttyout=>" -f - lagrange/ redis/ yunzai/ | gzip > $OUTPUT_NAME
+echo "]"
+echo "Done"
+chown $SUDO_USER:$(id -gn $SUDO_USER) $OUTPUT_NAME
+ls --color -hl "${OUTPUT_NAME}"*
