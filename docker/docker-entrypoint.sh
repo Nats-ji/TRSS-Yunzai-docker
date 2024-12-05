386739a36eb716314f70c79d812fcb494f45da7b
Ming <nats_ji@hotmail.com>
2024-12-05 09:22:36 +0800

    fix if statment

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -25,12 +25,12 @@ if [ "$(ls -A $CONFIG_DIR)" ]; then
     git clone --depth=1 https://github.com/yoimiya-kokomi/miao-plugin /app/TRSS-Yunzai/plugins/miao-plugin
     git clone --depth=1 https://github.com/TimeRainStarSky/TRSS-Plugin /app/TRSS-Yunzai/plugins/TRSS-Plugin
 
-    if [ -f "~/.INSTALL_GUOBA" ]; then
+    if [ -f "$HOME/.INSTALL_GUOBA" ]; then
         git clone --depth=1 https://github.com/guoba-yunzai/guoba-plugin.git /app/TRSS-Yunzai/plugins/Guoba-Plugin/
     fi
 fi
 
-echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 TRSS-Yunzai 更新 ${Font} \n ================ \n"
+echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 Yunzai 更新 ${Font} \n ================ \n"
 
 cd $WORK_DIR
 
@@ -45,11 +45,11 @@ else
 fi
 
 set -e
-echo -e "\n ================ \n ${Info} ${GreenBG} 更新 TRSS-Yunzai 运行依赖 ${Font} \n ================ \n"
+echo -e "\n ================ \n ${Info} ${GreenBG} 更新 Yunzai 运行依赖 ${Font} \n ================ \n"
 pnpm i
 set +e
 
-echo -e "\n ================ \n ${Version} ${BlueBG} TRSS-Yunzai 版本信息 ${Font} \n ================ \n"
+echo -e "\n ================ \n ${Version} ${BlueBG} Yunzai 版本信息 ${Font} \n ================ \n"
 
 git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
 
@@ -60,7 +60,7 @@ for PLUGIN in "${PLUGINS[@]}"; do
     PLUGIN_NAME=${PWD##*/}
 
     if [ -d .git ]; then
-        echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 $PLUGIN_NAME 更新 ${Font} \n ================ \n"
+        echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 $PLUGIN_NAME 插件更新 ${Font} \n ================ \n"
 
         if [[ -z $(git status -s) ]]; then
             echo -e " ${Warn} ${YellowBG} 当前工作区有修改，尝试暂存后更新。${Font}"
@@ -72,31 +72,31 @@ for PLUGIN in "${PLUGINS[@]}"; do
             git pull --allow-unrelated-histories
         fi
 
-        if [ -f "./package.json" ]
+        if [ -f "./package.json" ]; then
             set -e
-            echo -e "\n ================ \n ${Info} ${GreenBG} 更新 $PLUGIN_NAME 运行依赖 ${Font} \n ================ \n"
+            echo -e "\n ================ \n ${Info} ${GreenBG} 更新 $PLUGIN_NAME 插件运行依赖 ${Font} \n ================ \n"
             PKG_NAME=$(npm pkg get name)
 
             cd "$WORK_DIR"
-            yes | pnpm i --filter=$PKG_NAME
+            yes | pnpm i --filter="$PKG_NAME"
             cd "$PLUGIN"
             set +e
         fi
 
-        echo -e "\n ================ \n ${Version} ${BlueBG} $PLUGIN_NAME 版本信息 ${Font} \n ================ \n"
+        echo -e "\n ================ \n ${Version} ${BlueBG} $PLUGIN_NAME 插件版本信息 ${Font} \n ================ \n"
 
         git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
     fi
 done
 
-echo -e "\n ================ \n ${Info} ${GreenBG} 初始化 TRSS-Yunzai 设置 ${Font} \n ================ \n"
+echo -e "\n ================ \n ${Info} ${GreenBG} 初始化 Yunzai 设置 ${Font} \n ================ \n"
 
 if [ -f "./config/config/redis.yaml" ]; then
     sed -i 's/127.0.0.1/redis/g' ./config/config/redis.yaml
     echo -e "\n  修改Redis地址完成~  \n"
 fi
 
-echo -e "\n ================ \n ${Info} ${GreenBG} 启动 TRSS-Yunzai ${Font} \n ================ \n"
+echo -e "\n ================ \n ${Info} ${GreenBG} 启动 Yunzai ${Font} \n ================ \n"
 
 set +e
 cd "$WORK_DIR"
@@ -104,6 +104,6 @@ node app
 EXIT_CODE=$?
 
 if [[ $EXIT_CODE != 0 ]]; then
-	echo -e "\n ================ \n ${Warn} ${YellowBG} 启动 TRSS-Yunzai 失败 ${Font} \n ================ \n"
+	echo -e "\n ================ \n ${Warn} ${YellowBG} 启动 Yunzai 失败 ${Font} \n ================ \n"
 	tail -f /dev/null
 fi
