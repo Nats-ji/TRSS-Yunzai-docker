5d9baed8a92dee559d71c8df85a8e2971a12ff99
Ming <nats_ji@hotmail.com>
2024-12-05 20:53:38 +0800

    fix backup

--- backups/backup.sh
+++ backups/backup.sh
@@ -7,12 +7,12 @@ fi
 
 mkdir -p backups
 OUTPUT_NAME="./backups/yunzai-backup-$(date '+%Y-%m-%d').tar.gz"
-TOTAL_SIZE=`du -sk --apparent-size --total lagrange/ redis/ yunzai/ | cut -f 1 | tail -n 1`
+TOTAL_SIZE=`du -sk --apparent-size --total yunzai/ | cut -f 1 | tail -n 1`
 CHECKPOINT=`echo ${TOTAL_SIZE}/50 | bc`
 echo "Creating backup file. Total size: $(numfmt --to=iec $(echo ${TOTAL_SIZE}*1000 | bc))."
 echo "Estimated: [==================================================]"
 echo -n "Progess:   ["
-tar -c --record-size=1K --checkpoint="${CHECKPOINT}" --checkpoint-action="ttyout=>" -f - lagrange/ redis/ yunzai/ | gzip > $OUTPUT_NAME
+tar -c --record-size=1K --checkpoint="${CHECKPOINT}" --checkpoint-action="ttyout=>" -f - yunzai/ | gzip > $OUTPUT_NAME
 echo "]"
 echo "Done"
 chown $SUDO_USER:$(id -gn $SUDO_USER) $OUTPUT_NAME
